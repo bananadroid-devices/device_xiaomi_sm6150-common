0742a326ae12cde2afb6fd629bb42828814e6499
Cyber Knight <cyberknight755@gmail.com>
2022-06-17 02:12:27 +0800

    sm6150-common: Build KProfiles
    - Inherit KProfiles.
    - Address sepolicy.
    - Define sysfs_kprofiles as a domain.
    - Fix permission of nodes relevant to KProfiles at boot.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -251,6 +251,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
