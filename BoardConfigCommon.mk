43acee13b08102f7316212fb2387ceb5a2db2fb1
Suren Baghdasaryan <surenb@google.com>
2022-07-18 17:06:06 -0700

    sm6150-common: Disable per-cgroup PSI accounting
    Per-cgroup PSI accounting is not used in Android while having small
    performance cost. Disable this unused feature.
    
    Bug: 233410456
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ib5174d7d819e33e2fc2f71bfafb68e8f4b79631a
    Merged-In: Ib5174d7d819e33e2fc2f71bfafb68e8f4b79631a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,6 +87,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
 
 # Enable DTB in bootimage and set header version
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
