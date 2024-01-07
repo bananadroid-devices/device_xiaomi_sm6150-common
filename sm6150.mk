5917e214fac4a429bd79f7f82a6bf9e05677ffbf
Pranav Vashi <neobuddy89@gmail.com>
2024-01-07 18:15:43 +0530

    sm6150-common: Add dolby to cloned apps exemption list
    Change-Id: I47ab06f7d5152c4c993359ac85d121e41c8df235
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -114,6 +114,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Clone apps exemption
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/preinstalled-packages-platform-xiaomi-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-xiaomi-product.xml
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
